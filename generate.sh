5f72361be5af769cbad10b3ea43c2046b9ab23d7
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-01-22 15:37:28 +0300

    Format script

--- generate.sh
+++ generate.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-bindgen --opaque-type=FILE --blacklist-type='^_.*' --whitelist-function='^opj.*' --whitelist-type='^opj.*' \
- --whitelist-var='^opj.*' --distrust-clang-mangling  --no-layout-tests --rust-target=1.26 --rustified-enum='.*' \
- vendor/src/lib/openjp2/openjpeg.h -- -Ivendor/src/src/lib/openjp2/ -Iconfig/ |
-  sed -E 's/pub type FILE.*/use libc::FILE;/;
-  s/ @param +([^ ]+)/ * `\1` — /;
-  s/pub type OPJ_BYTE.*/pub type OPJ_BYTE = u8;/;
-  2s/^/use std::os::raw::*;/;
-  s/::std::os::raw:://g' > src/ffi.rs
+
+bindgen --opaque-type=FILE \
+        --blacklist-type='^_.*' \
+        --whitelist-function='^opj.*' \
+        --whitelist-type='^opj.*' \
+        --whitelist-var='^opj.*' \
+        --rust-target=1.26 \
+        --rustified-enum='.*' \
+        --distrust-clang-mangling --no-layout-tests \
+        vendor/src/lib/openjp2/openjpeg.h -- -Ivendor/src/src/lib/openjp2/ -Iconfig/ | 
+        sed -E "s/pub type FILE.*/use libc::FILE;/; 
+                s/ @param +([^ ]+)/ * '\1' — /; 
+                s/pub type OPJ_BYTE.*/pub type OPJ_BYTE = u8;/;
+                2s/^/use std::os::raw::*;/;
+                s/::std::os::raw:://g" > src/ffi.rs
